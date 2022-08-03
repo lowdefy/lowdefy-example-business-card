336e6117389dcc736c812ce6e878ea801fd3846d
Sam <sam@lowdefy.com>
2022-08-03 18:09:46 +0200

    fix: Remove dist files.

--- plugins/plugin-business-cards/dist/types.js
+++
@@ -1,8 +0,0 @@
-import * as adapters from './auth/adapters.js';
-import * as blocks from './blocks.js';
-export default {
-    auth: {
-        adapters: Object.keys(adapters)
-    },
-    blocks: Object.keys(blocks)
-};
