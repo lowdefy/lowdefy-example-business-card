9fc2079f438affe14f73a25e61342c217eab208e
Sam <sam@lowdefy.com>
2022-08-03 23:06:32 +0200

    fix: Fix home page QR code.

--- plugins/plugin-business-cards/src/types.js
+++ plugins/plugin-business-cards/src/types.js
@@ -1,9 +1,8 @@
 import * as adapters from './auth/adapters.js';
-import * as blocks from './blocks.js';
 
 export default {
   auth: {
     adapters: Object.keys(adapters),
   },
-  blocks: Object.keys(blocks),
+  blocks: ['QRCode'],
 };
